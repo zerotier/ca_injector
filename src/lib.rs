259f7c4e9c5873f88b2e7e5392de0a7e8304a752
Erik Hollensbe <git@hollensbe.org>
2022-02-28 02:31:17 -0800

    docs
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
+//! ca_injector: inject CAs into a variety of trust stores
+//!
+//! This code is adapted from [mkcert](https://github.com/FiloSottile/mkcert) and presented as a
+//! library. The general idea is to support a variety of needs for installation of third party CA
+//! certificates into trust stores.
+//!
+//! Please see the (arguably simple) API below to meet your needs. This library only supports linux
+//! as of this writing, but OS X and Windows support are planned in the near future.
 #[cfg(target_os = "linux")]
 mod linux;
 
+/// This function installs a file by name into the system and NSS trust stores. For NSS, the CA
+/// name will also be the filename. For system trust stores, this is highly dependent on platform
+/// and "platform flavor", such as Ubuntu or Debian for Linux.
 pub fn install_ca(filename: &str) -> Result<(), anyhow::Error> {
     #[cfg(target_os = "linux")]
     return crate::linux::install_ca(filename);
@@ -12,6 +23,8 @@ pub fn install_ca(filename: &str) -> Result<(), anyhow::Error> {
     ))
 }
 
+/// The inverse of [install_ca]. Must be presented with the same base filename as [install_ca] to
+/// remove. Errors will occur if the file is not found.
 pub fn uninstall_ca(filename: &str) -> Result<(), anyhow::Error> {
     #[cfg(target_os = "linux")]
     return crate::linux::uninstall_ca(filename);
